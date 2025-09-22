850e1b2dd1d0e1c84439e7e9f2c7408277ff6d52
Soyam14 <zues60860@gmail.com>
2025-09-22 16:30:16 +0530

    Configure PostgreSQL database for production

--- MERI_DIET/settings.py
+++ MERI_DIET/settings.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 import os # Make sure this is imported at the top
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,10 +64,11 @@ WSGI_APPLICATION = 'MERI_DIET.wsgi'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 
