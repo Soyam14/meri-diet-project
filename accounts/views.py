80b45668b2199d55f3b303c99d75d2bfc2451643
tiwarisourav860 <tiwarisourav860@gmail.com>
2025-10-21 10:50:32 +0530

    finally

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-# accounts/views.py
+# MyDietApp/accounts/views.py
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
@@ -11,11 +12,17 @@ def signup_view(request):
     if request.method == 'POST':
         form = CustomSignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            
+            # This is the fix for the IntegrityError.
+            # It creates the Profile associated with the new user.
             Profile.objects.create(
                 user=user,
                 phone_number=form.cleaned_data['phone_number']
             )
+            
             login(request, user)
             return redirect('home')
     else:
